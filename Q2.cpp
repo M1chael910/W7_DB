7866385aceb7e3a878498b44b77341d227197f99
Michael Murphy <mpmurphybusiness@gmail.com>
2023-03-05 18:08:30 -0600

    initial commit

---
+++ Q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int xvalue(int);
+int yreference(int);
+
+
+int main() {
+    int x = 0;
+    int y = 0;
+    std::cout << "Enter two numbers: " << std::endl;
+    std::cin >> x;
+    std::cin >> y;
+
+    return 0;
+}
