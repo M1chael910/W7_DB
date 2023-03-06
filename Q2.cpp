26f27e68769984eb32a6fa8a8686a8511a32e248
Michael Murphy <mpmurphybusiness@gmail.com>
2023-03-05 20:02:01 -0600

    finished Q2

--- Q2.cpp
+++ Q2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int xvalue(int);
-int yreference(int);
+int xvalue(int x);
+int yreference(int& y);
 
 
 int main() {
@@ -10,6 +10,15 @@ int main() {
     std::cout << "Enter two numbers: " << std::endl;
     std::cin >> x;
     std::cin >> y;
-
+    std::cout << "Your first squared is number: " << xvalue(x) << std::endl;
+    std::cout << "Your second number squared is: " << yreference(y) << std::endl;
     return 0;
+}
+
+int xvalue(int x) {
+    return x*x;
+}
+
+int yreference(int& y) {
+    return y*y;
 }
